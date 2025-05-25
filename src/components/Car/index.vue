f2b17f68729bd90b7774b1532b870efad6ed72d8
ClungTsang <shawn1440982358@163.com>
2025-05-25 17:07:13 +0800

    refactor(组件/Car): 将iframe替换为本地图片以简化预览
    为了简化预览流程并减少对本地服务器的依赖，将iframe替换为本地图片。这样可以避免因服务器未启动导致的预览问题，并提高开发效率。

--- src/components/Car/index.vue
+++ src/components/Car/index.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- <div id="car" class="w-screen h-screen"></div> -->
-   <iframe src="http://localhost:8000//zqlby-HTML5-Shipping.html" class="w-screen h-screen bg-black"></iframe>
+  <!-- <iframe src="http://localhost:8000//zqlby-HTML5-Shipping.html" class="w-screen h-screen bg-black"></iframe> -->
+  <img src="@/assets/temp/image.png" class="w-screen h-screen" />
 </template>
 
 <script setup lang="ts">
